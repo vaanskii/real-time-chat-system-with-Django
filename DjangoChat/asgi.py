2816a960a658fe96beefa663e352374e37cae47d
vaanskii <giorgivanadze03@gmail.com>
2024-03-29 19:03:53 +0400

    Integrate changes. Implemented WebSocket functionality for managing group chat in Django using Channels. Added ChatConsumer class extending AsyncWebsocketConsumer to handle WebSocket connections. The connect method adds channels to a specified group, disconnect method removes channels from the group upon disconnection. The receive method processes incoming messages and broadcasts them to the group. Also defined a sendMessage method to send messages to connected clients. Included WebSocket URL patterns in the routing configuration.

--- DjangoChat/asgi.py
+++ DjangoChat/asgi.py
@@ -1,16 +1,18 @@
-"""
-ASGI config for DjangoChat project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
+from channels.routing import ProtocolTypeRouter, URLRouter
+from ChitChat import routing
+from channels.auth import AuthMiddlewareStack
 import os
-
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoChat.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                routing.websocket_urlpatterns
+            )
+        )
+    }
+)
